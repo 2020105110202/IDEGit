55a0f9a5410ee3f0625601ef8ad75832ad4a5e6a
Big_cat <big_cat1555@163.com>
2021-10-22 22:01:07 +0800

    添加乘法

--- IdeGit/src/main/java/com/student/text/Calculateor.java
+++ IdeGit/src/main/java/com/student/text/Calculateor.java
@@ -30,6 +30,14 @@ class Sub implements ICalculate{
         return result;
     }
 }
+class Mul implements ICalculate{
+    private int result;
+    public int calculate(int m,int n){
+        result=m*n;
+        System.out.println(m+"*"+n+"="+result);
+        return result;
+    }
+}
 public class Calculateor {
     public static void main(String[] args) {
         int op1;
